d70a7c70697856263c44addf23f50711b39d1822
master2013 <372006848@qq.com>
2018-03-15 11:48:02 +0800

    get,post请求

--- src/main/java/com/sprbt/controller/HomeController.java
+++ src/main/java/com/sprbt/controller/HomeController.java
@@ -2,6 +2,10 @@ package com.sprbt.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +25,17 @@ public class HomeController {
 		return  ary;
 	}
 	
+	
+//	@PathVariable 是url中的参数，RequestParam 是请求中的参数  key要对应
+	@RequestMapping(value = "/user/{userid}",method = RequestMethod.GET,consumes = "application/json")
+	public String getUserInfo(@PathVariable String userid, @RequestParam String pwd) {
+		
+		return "welcome," + userid;
+	}
+	
+	@RequestMapping(value = "/user/login", method = RequestMethod.POST ,consumes = "application/json")
+	public String sendLogin(@RequestParam String name, @RequestParam String password) {
+	
+		return "userName:" + name + "\n" + "password:" + password;
+	}
 }
